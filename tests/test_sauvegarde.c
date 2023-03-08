a35e53667843652bd52cb4a3a906368def2976b2
DamiBoul <damien.boulanger.etu@univ-lemans.fr>
2023-03-08 11:45:06 +0100

    test sauvegardes

---
+++ tests/test_sauvegarde.c
@@ -0,0 +1,55 @@
+#include "../lib/morpion.h"
+#include "../lib/sauvegarde.h"
+
+int main(){
+
+    int grille[N][N];
+    int morpion[M][M];
+
+    printf("============ Début du test pour les fonctions save & load\n");
+    init_grille(grille);
+    grille[3][4]=1;
+    grille[0][0]=2;
+    grille[0][1]=2;
+    grille[0][2]=2;
+    printf("Grille avant sauvegarde :\n");
+    afficher_grille(grille);
+    init_morpion(morpion);
+    morpion[0][0]=2;
+    printf("Morpion avant sauvegarde :\n");
+    afficher_morpion(morpion);
+    printf("Sauvegarde avec joueur=1 et (x,y) dernier coup=(3,4)\n\n");
+    save("test_save1",grille,morpion,1,3,4);
+    FILE * slot = fopen("test_save1","r");
+    char savedata[92];
+    fscanf(slot,"%s\n",savedata);
+    printf("Données de sauvegarde 1 = %s\n",savedata);
+    printf("Grille après sauvegarde (ne doit pas être modifiée) :\n");
+    afficher_grille(grille);
+    printf("Morpion après sauvegarde (ne doit pas être modifié) :\n");
+    afficher_morpion(morpion);
+    fclose(slot);
+
+    printf("\n===== Test chargement\n\n");
+    init_grille(grille);
+    init_morpion(morpion);printf("Grille avant chargement :\n");
+    afficher_grille(grille);
+    printf("Morpion avant chargement :\n");
+    afficher_morpion(morpion);
+    int joueur,xdc,ydc;
+    printf("Initialistion variables de jeu, joueur = %i et coordonnées dernier coup = (%i, %i)\n",joueur,xdc,ydc);
+    printf("Chargement avec fichier test_save2\n");
+    slot = fopen("test_save2","r");
+    fscanf(slot,"%s\n",savedata);
+    printf("Données de sauvegarde 2 = %s\n",savedata);
+    load("test_save2",grille,morpion,&joueur,&xdc,&ydc);
+    printf("Nouvelles variables de jeu, joueur = %i et coordonnées dernier coup = (%i, %i)\n",joueur,xdc,ydc);
+    printf("Grille après chargement :\n");
+    afficher_grille(grille);
+    printf("Morpion après chargement :\n");
+    afficher_morpion(morpion);
+    fclose(slot);
+
+
+    return 0;
+}
